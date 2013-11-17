4acfededa149f60b0384ffb2c6f64f3db6699513
Avinash Gosavi <aag1091@gmail.com>
2013-11-17 12:39:27 +0530

    Added rails new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sgsexpress::Application.config.secret_key_base = '13151b4b7d9f7f9df87389a25247ef0a3dad3317b894fa4c0812824d9da98b919d522a801f86d6eba0a41fb4bf97ac7d5a6831a78e03bf1148d89dacaa9f0781'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sgsexpress::Application.config.session_store :cookie_store, key: '_sgsexpress_session'

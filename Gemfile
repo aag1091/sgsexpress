b66f533c27356f5c0ada2cbefb7d94c593381fa5
Avinash Gosavi <aag1091@gmail.com>
2013-12-01 16:31:11 +0530

    added high voltage and some static pages

--- Gemfile
+++ Gemfile
@@ -22,7 +22,7 @@ gem 'coffee-rails', '~> 4.0.0'
 gem 'jquery-rails'
 
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
-gem 'turbolinks'
+# gem 'turbolinks'
 
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 1.2'
@@ -69,3 +69,6 @@ end
 
 gem 'devise'
 gem 'rails_admin'
+gem 'zurb-foundation'
+gem 'haml'
+gem 'high_voltage'
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Sgsexpress::Application.routes.draw do
-  get "home/index"
+
+  get 'home/index'
+
+  root 'home#index'
+
   devise_for :users
+
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -42,7 +48,7 @@ Sgsexpress::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'

8e43e07722c67e11022b90376d78a1a8b2be28fd
Avinash Gosavi <aag1091@gmail.com>
2014-07-29 11:15:16 +0530

    resolved place issue

--- app/helpers/shipments_helper.rb
+++ app/helpers/shipments_helper.rb
@@ -5,7 +5,7 @@ module ShipmentsHelper
   end
 
   def place_display(user_detail)
-    user_detail ? [user_detail.locality, user_detail.city, user_detail.state].compact.join(', ') : "Not Set"
+    user_detail ? ([user_detail.locality, user_detail.city, user_detail.state].compact.uniq - [""]).join(', ') : "Not Set"
   end
 
 end

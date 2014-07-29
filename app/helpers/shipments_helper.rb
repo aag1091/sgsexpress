8540324315e34ed05126afa8377a434453ec25a4
Avinash Gosavi <aag1091@gmail.com>
2014-07-29 11:11:54 +0530

    date not set issue resolved

--- app/helpers/shipments_helper.rb
+++ app/helpers/shipments_helper.rb
@@ -1,2 +1,11 @@
 module ShipmentsHelper
+
+  def date_display(ship_date)
+    ship_date ? ship_date.strftime("%d/%m/%Y") : 'Not Set'
+  end
+
+  def place_display(user_detail)
+    user_detail ? [user_detail.locality, user_detail.city, user_detail.state].compact.join(', ') : "Not Set"
+  end
+
 end
